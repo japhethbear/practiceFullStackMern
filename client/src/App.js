bf48632cf71e0289f14a23c30b1e898e109a19bc
japhethbear <japhethbear@gmail.com>
2023-03-14 09:55:10 -0500

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PersonForm from './components/PersonForm';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <PersonForm/>
+    </div>
+  );
+}
+
+export default App;
